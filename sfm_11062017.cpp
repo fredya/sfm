00231caaf6a2c3793edfa7d5d0e9e0d01bfd6fe4
fredya <yazhehu@vt.edu>
2017-11-09 16:15:32 -0500

    Using FileStorate

--- sfm_11062017.cpp
+++ sfm_11062017.cpp
@@ -12,10 +12,23 @@ using namespace cv;
 
 int main()
 {
+	//load cameraMatrix, distCoeffs
+	FileStorage fs("out_camera.xml", FileStorage::READ);
+	fs["camera_matrix"] >> cameraMatrix;
+	fs["distortion_coefficients"] >> distCoeffs;
+	cout << "camera_matrix" << cameraMatrix;
+
+	//read images from source
 	Mat im1 = imread("f1.jpg", CV_LOAD_IMAGE_COLOR);
 	imshow("a", im1);
 	Mat im2 = imread("f2.jpg", CV_LOAD_IMAGE_COLOR);
 	imshow("b", im2);
+
+	//undistort images
+	Mat temp, view;
+	temp = im1;
+	undistort(temp, view, cameraMatrix, distCoeffs);
+
 
 	waitKey(0);
 	return 0;
